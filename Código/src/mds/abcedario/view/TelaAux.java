06b7feb7f36a862c89cfbd983cbd5ae638602d1b
lucaskanashiro <lucas-kanashiro@hotmail.com>
2013-01-08 23:04:27 -0200

    Update Código/src/mds/abcedario/view/TelaAux.java

--- Código/src/mds/abcedario/view/TelaAux.java
+++ Código/src/mds/abcedario/view/TelaAux.java
@@ -1,16 +1,10 @@
 package mds.abcedario.view;
 
-import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v4.widget.SimpleCursorAdapter;
 import fga.mds.abcedario.R;
-import mds.abcedario.view.superclasses.AbstractActivity;
 
-public class TelaAux extends ListActivity {
-	
-	SimpleCursorAdapter mAdapter;
-	
-	static final String[] infos = {"Nome: ABCedário", "Versão: 1.0"};
+public class TelaAux extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
